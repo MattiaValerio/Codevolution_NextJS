6f63cc81f5c6058ac6d2c68f124bae2d22c0360e
MattiaValerio <mattia.valerio2002@gmail.com>
2023-12-08 13:46:21 +0100

    add todos

--- src/ServerActions/userServerActions.tsx
+++ src/ServerActions/userServerActions.tsx
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 
-export default async function addUserAction(e: FormData) {
+export async function addUserAction(e: FormData) {
       const dati = e.get("dati")?.toString()
 
       const x = JSON.parse(dati!)
@@ -14,9 +14,20 @@ export default async function addUserAction(e: FormData) {
       await prisma.users.create({
             data: x
       })
+}
 
+export async function AddTodo(e: FormData) {
+      const dati = e.get("dati")?.toString()
+      const x = JSON.parse(dati!)
 
+      console.log(x)
 
+      await prisma.todos.create({ data: x })
+}
 
+export async function GetTodos() {
+      const todos = await prisma.todos.findMany()
+
+      return todos;
 }
 
